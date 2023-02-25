932af17cd0ebc22f343529fb32d0193024ebe856
Siva <sivar15racer@gmail.com>
2023-02-25 20:10:28 +0530

    modified

--- main.js
+++ main.js
@@ -1,61 +1,29 @@
+const input = document.querySelectorAll(".box");
 
-let box=document.querySelectorAll(".box");
+let row=0;
 
-
-
-
-// checking valid letters
-let isLetter=(input)=> {
-    return /^[a-zA-Z]$/.test(input);
-  }
-
-  box.innerText=5;
-async function init()
-{
-    let row=0;
-    let currentWord="";         //user typing word
-
-    //fetch the word
-
-    const fetchResult=await fetch("https://words.dev-apis.com/word-of-the-day");
-    const obj=await fetchResult.json();
-    let words=obj.word;
-    words=words.toUpperCase();
-    let wordParts=words.split("");
+document.addEventListener("keydown",function handleKeyPress(event){
+    const pressed=event.key;
+    
+    if(pressed==="Enter") ent();
+    else if(pressed==="Backspace")backspace();
+    else if(isLetter(pressed)){
+        addLetter(pressed.toUpperCase());
+    }
+});
+current="";
+let addLetter=inp =>{
 
 
-    let addLetter=input =>
+    if(current.length<5)        // not fill
     {
-        if(currentWord.length<5)
-        {
-            currentWord +=input;
-        }else {
-            currentWord=currentWord.substring(0,currentWord.length-1)+input;
-        }
-        box[row*5+currentWord.length-1].innerText=input;
+    input[row].innerText=inp;
+        row++;
     }
-
-    async function commit (){               //pressed enter
-
-        if(currentWord.length !==5){
-            return;
-        }
-            const inputWord=currentWord.split("");
-            
-    }
-  
-
-    document.addEventListener("keydown",function handleKeyPress(event){
-        const pressed =event.key;
-
-        if(pressed =="Enter") commit();
-        else if(pressed="Backspace") backspace();
-        else if(isLetter(pressed)) addLetter(pressed.toUpperCase());
-        
-    });
-
 }
 
-  init();
-
+let isLetter=inp =>
+{
+    return /^[a-zA-Z]$/.test(inp);
+}
 
