c38f0a60d1daaac8cff8f7e9663041f6ac41db3a
Siva <sivar15racer@gmail.com>
2023-02-26 02:49:57 +0530

    arranged better and renamed variables

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-const input = document.querySelectorAll(".box");
+const letters = document.querySelectorAll(".box");
 
-let row=0;
+let boxNumber=0;
+let numberOfChances=5;
+let secretWord="";
+let arrayOfSecretWord;
+async function fetchWord()
+{
+    const result=await fetch("https://words.dev-apis.com/word-of-the-day");
+    let {word:inp} =await result.json();
+    secretWord=inp.toUpperCase();
+    arrayOfSecretWord=secretWord.split("");
+}
 
-let word="MONEY";
+fetchWord();
+
 document.addEventListener("keydown",function handleKeyPress(event){
     const pressed=event.key;
     
@@ -12,36 +23,67 @@ document.addEventListener("keydown",function handleKeyPress(event){
         addLetter(pressed.toUpperCase());
     }
 });
-current="";
-let addLetter=inp =>{
+
+currentWord="";
+let addLetter=value =>{
 
 
-    if(current.length<5)        // not fill
+    if(currentWord.length<5)        
     {
-    input[row].innerText=inp;
-        row++;
-        current +=inp;
+    letters[boxNumber].innerText=value;
+        currentWord +=value;
+
+    boxNumber++;
+
     }else {
-        // press enter to current="" & row++;
-        input[row-1].innerText=inp;
+        
+        letters[boxNumber-1].innerText=value;
+        currentWord=currentWord.substring(0,currentWord.length-1);
+        currentWord +=value;
+
     }
 }
 
 
-let isLetter=inp =>
+let isLetter=value =>
 {
-    return /^[a-zA-Z]$/.test(inp);
+    return /^[a-zA-Z]$/.test(value);
 }
-let ent=inp =>
+
+
+let ent=value =>
 {
-    if(current==word)
+    if(currentWord==secretWord)
     {
         alert("you win");
-    }else 
-    {
-        // pressed enter but word is not right
-        current="";
+        
+    }else if(currentWord.length==5) {   
+            if(numberOfChances==0) alert("you lose");
+            numberOfChances--;
+            let loop=boxNumber-5;
+            let currentWordArray=currentWord.split("");
+            for(loop;loop<boxNumber;loop++)
+            {
+                if(arrayOfSecretWord[loop%5]==currentWordArray[loop%5])
+                {
+                    letters[loop].style.backgroundColor="green";
+                }else 
+            letters[loop].style.backgroundColor="grey";
+            }
+            currentWord="";
+
     }
 }
-
+let backspace=value =>
+    {
+    if(currentWord.length<1)
+    {
+        return;
+    }else 
+    {
+        currentWord=currentWord.substring(0,currentWord.length-1);
+        letters[boxNumber-1].innerText="";
+        if(boxNumber!==0)boxNumber--;
+    }
+    }
 
